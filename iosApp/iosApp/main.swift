969796f2535a4ecb10bc66ae50e24e11cdb1d889
michael.thiele <thiele.michael2@gmail.com>
2026-02-24 05:46:00 +0100

    iOS: depending on target, an ENV variable is used (SCREENSHOTS) to trigger a different app

--- iosApp/iosApp/main.swift
+++ iosApp/iosApp/main.swift
@@ -1,3 +1,11 @@
 import TammyUI
 
-try MainKt.main(args: CommandLine.arguments)
+if let value = ProcessInfo.processInfo.environment["SCREENSHOTS"] {
+    if (value == "YES") {
+        print("Special runner for making screenshots!")
+        // TODO special version
+        try MainKt.main(args: CommandLine.arguments)
+    } else {
+        try MainKt.main(args: CommandLine.arguments)
+    }
+}
