98add3a47965fa8ecc346c81366462b4b8a0598d
michael.thiele <thiele.michael2@gmail.com>
2026-02-26 05:23:42 +0100

    iOS: load screenshots as images first, then convert to ByteArray

--- src/appleTest/kotlin/de/connect2x/tammy/screenshot/resource.apple.kt
+++
@@ -1,7 +0,0 @@
-package de.connect2x.tammy.screenshot
-
-actual object PlatformResource {
-    actual fun resource(path: String): ByteArray? {
-        TODO("Not yet implemented")
-    }
-}
--- src/iosMain/kotlin/de/connect2x/tammy/screenshot/resource.ios.kt
+++ src/iosMain/kotlin/de/connect2x/tammy/screenshot/resource.ios.kt
@@ -1,14 +1,18 @@
 package de.connect2x.tammy.screenshot
 
-import de.connect2x.trixnity.messenger.util.toByteArray
+import de.connect2x.trixnity.messenger.compose.view.room.timeline.element.details.toByteArray
 import platform.Foundation.NSBundle
-import platform.Foundation.NSData
-import platform.Foundation.dataWithContentsOfFile
+import platform.UIKit.UIImage
 
 actual object PlatformResource {
     actual fun resource(path: String): ByteArray? {
+        // for some unknown reason, we cannot load the file's byte data directly as iOS will change the png when copying
+        // it to the bundle (e.g. inflate the size to nearly double the size). The pure read bytes method does then not
+        // produce a ByteArray that Compose can interprete as an image.
+        // So instead, load it as an image on iOS, convert it back to ByteArray, to then convert it to Compose later.
         val bundle = NSBundle.mainBundle
         val path = bundle.pathForResource(path.split(".")[0], path.split(".")[1]) ?: error("Unable to locate resource")
-        return NSData.dataWithContentsOfFile(path)?.toByteArray() ?: error("Unable to read resource")
+        val image = UIImage.imageWithContentsOfFile(path)
+        return image?.toByteArray(1.0)
     }
 }
